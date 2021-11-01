c97de9811f30e1d40d3e632685ec06b58ffebddb
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-01 09:20:13 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BarChart v-bind:someData="someData" v-bind:currencyArray="currencyArray" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BarChart from "./components/BarChart.vue";
+let url =
+  "	https://sheltered-scrubland-49038.herokuapp.com/https://api.coindesk.com/v1/bpi/currentprice.json";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    BarChart,
+  },
+  data() {
+    return {
+      someData: "",
+      currencyArray: [],
+    };
+  },
+  async beforeCreate() {
+    try {
+      let res = await fetch(url);
+      console.log(res);
+      let data = await res.json();
+      // console.log(data.bpi);
+      // wholeData = data.bpi;
+      console.log(data.bpi);
+      // this.someData = data.bpi;
+      for (let currency in data.bpi) {
+        // console.log(data.bpi[currency].code);
+        this.currencyArray.push(data.bpi[currency].code);
+      }
+      // barChart.data.labels = names.value;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/BarChart.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineComponent } from "vue";
+import { Bar } from "vue3-chart-v2";
+
+export default defineComponent({
+  name: "MonthlyChart",
+  extends: Bar,
+  props: ["someData", "currencyArray"],
+  data() {
+    return {
+      currency: "",
+      Data: null,
+    };
+  },
+
+  mounted() {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: this.currencyArray,
+      datasets: [
+        {
+          label: "GitHub Commits",
+          backgroundColor: "#f87979",
+          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+        },
+      ],
+    });
+    // console.log(this.Data);
+    // console.log(this.renderChart);
+    // console.log(this.someData);
+    console.log(this.currencyArray);
+  },
+});
+</script>
